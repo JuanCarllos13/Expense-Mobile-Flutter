d928c0217166e25877514969f826aee0f058ce0c
Juan Carlos <juan2017amaral@gmail.com>
2023-08-02 18:11:08 -0400

    feat: create list expenses

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+
+  final List<Transaction> transaction;
+
+  TransactionList(this.transaction, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+}
